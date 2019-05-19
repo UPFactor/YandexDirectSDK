3a28827a81e724181430db6f99408a0503a6238d
ivanmiroshin <ivanmiroshin@gmail.com>
2019-05-19 09:47:59 +0300

    v1.2.0

--- tests/Examples/AdGroupsExamplesTest.php
+++ tests/Examples/AdGroupsExamplesTest.php
@@ -4,8 +4,41 @@
 namespace YandexDirectSDKTest\Examples;
 
 use PHPUnit\Framework\TestCase;
+//use YandexDirectSDKTest\Helpers\SessionTools;
 
 class AdGroupsExamplesTest extends TestCase
 {
+    /*
+     |-------------------------------------------------------------------------------
+     |
+     | Add campaigns
+     |
+     |-------------------------------------------------------------------------------
+    */
+
+    public function testAddAdGroup_byService(){
+        //$session = SessionTools::init();
+
+        // Demo =====================================================================
+
+        /*$result = $session->getAdGroupsService()->add(
+            AdGroups::make()->push(
+                AdGroup::make()
+
+            )
+        );*/
+    }
+
+    public function testAddAdGroup_byModel(){
+
+    }
+
+    public function testAddAdGroup_byCollection(){
+
+    }
+
+    public function testAddAdGroup_byArray(){
+
+    }
 
 }
